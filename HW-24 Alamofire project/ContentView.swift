e57726a8409b18afd5db0243ad38817f67078b2d
DimkaVinka <dimkavinka@gmail.com>
2022-04-14 11:28:26 +0300

    Initial Commit

---
+++ HW-24 Alamofire project/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HW-24 Alamofire project
+//
+//  Created by Дмитрий Виноградов on 14.04.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HW-24 Alamofire project/HW_24_Alamofire_projectApp.swift
@@ -0,0 +1,17 @@
+//
+//  HW_24_Alamofire_projectApp.swift
+//  HW-24 Alamofire project
+//
+//  Created by Дмитрий Виноградов on 14.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct HW_24_Alamofire_projectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
