7e5caa5e11028c7ecc3ad81166f3d0ab8b045142
DimkaVinka <dimkavinka@gmail.com>
2022-04-17 21:35:43 +0300

    made some funcionality in CellView

--- HW-24 Alamofire project/Views/CellView.swift
+++ HW-24 Alamofire project/Views/CellView.swift
@@ -5,4 +5,36 @@
 //  Created by Дмитрий Виноградов on 14.04.2022.
 //
 
-import Foundation
+import SwiftUI
+
+struct CellView: View {
+    
+    @State var image = "landscape_xlarge"
+    @State var name = "Some character"
+    
+    var body: some View {
+        ZStack {
+            Color.init(hex: "#EC1D24")
+                .edgesIgnoringSafeArea(.all)
+            RoundedRectangle(cornerRadius: 20)
+                .frame(width: 300, height: 90, alignment: .center)
+                .foregroundColor(Color.white)
+                .shadow(radius: 7)
+            HStack(alignment: .center) {
+                Image(image)
+                    .resizable()
+                    .frame(width: 120, height: 80, alignment: .leading)
+                    .cornerRadius(20)
+                    .shadow(radius: 5)
+                Text(name)
+                Spacer()
+            }.padding(.horizontal)
+        }
+    }
+}
+
+struct CellView_Previews: PreviewProvider {
+    static var previews: some View {
+        CellView()
+    }
+}
--- HW-24 Alamofire project/Views/TableView.swift
+++ HW-24 Alamofire project/Views/TableView.swift
@@ -8,9 +8,33 @@
 import SwiftUI
 
 struct TableView: View {
+    
+    @ObservedObject var controller = Controller()
+    @State var show = false
+    @State private var columns = [
+        GridItem(.flexible())
+    ]
 
     var body: some View {
-        Text("TableView")
+        ZStack {
+            Color.init(hex: "#EC1D24")
+                .edgesIgnoringSafeArea(.all)
+            ScrollView(.vertical, showsIndicators: false) {
+                ForEach(controller.spiderManData, id: \.id) { card in
+                    CellView(image: "", name: "")
+                }
+            }
+        }
+        
+        if show {
+            ContentView()
+                .transition(.asymmetric(insertion: .opacity, removal: .scale))
+        }
+        
+    }
+    
+    func makeImageURL(path: String, extensionToPath: String) -> String {
+        return path + "/landscape_xlarge." + extensionToPath
     }
 }
 
